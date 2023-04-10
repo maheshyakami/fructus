cb2951163df9b7d5cb642422437a1f729111fa39
Mahesh Yakami <yakami.mahesh@gmail.com>
2023-04-10 15:20:09 +0545

    project init

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Mahesh Yakami on 10/04/2023.
+//  
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
